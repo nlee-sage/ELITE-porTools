01b1a48493dc3176a89517ea570be8beebceab79
Nicole Kauer <nmekauer@gmail.com>
2021-09-10 12:31:11 -0700

    Update script for AD pubs for GH Action, add Action, add pub Rmd

--- inst/scripts/update-publications-ad.R
+++ inst/scripts/update-publications-ad.R
@@ -12,43 +12,18 @@
 ##   Rscript update-publications-ad.R \              ##
 ##     --grant_table <synID> \                       ##
 ##     --parent <synID> \                            ##
-##     --log_dir <synID> \                           ##
 ##     [--auth_token <Synapse PAT>] \                ##
 ##     [--pub_table <synID>] \                       ##
-##     [--task_id <synID>] \                         ##
-##     [--task_table <synID>]                        ##
 ##                                                   ##
 #######################################################
 
 ## Libraries -------------------------------------------------------------------
 
-suppressPackageStartupMessages(library("dplyr"))
-suppressPackageStartupMessages(library("log4r"))
-suppressPackageStartupMessages(library("lubridate"))
-suppressPackageStartupMessages(library("optparse"))
-suppressPackageStartupMessages(library("porTools"))
+library("dplyr")
+library("optparse")
+library("porTools")
 ## Required, but not fully loaded
 ## readr, reticulate, glue, easyPubMed, dccvalidator
-
-## Helper function to store task annotation ------------------------------------
-
-#' Update task annotation
-#'
-#' Update the 'completed_successfully' task annotation on a Synapse folder.
-#' Expected that the annotation is a text type.
-#'
-#' @param syn Synapse client object
-#' @param annots Synapse annotation object for the task folder
-#' @param success TRUE if the task was completed successfully, else FALSE
-#' @param task_view synID for the task file view (default: `NA`)
-update_task_annotation <- function(syn, annots, success, task_view = NA) {
-  annots['completed_successfully'] <- success
-  syn$set_annotations(annots)
-  ## Force file view update, if task_view is given
-  if (!is.na(task_view)) {
-    syn$tableQuery(glue::glue("SELECT * FROM {task_view}"))
-  }
-}
 
 ## Setup -----------------------------------------------------------------------
 
@@ -81,116 +56,29 @@ option_list <- list(
     default = NA,
     type = "character",
     help = "Synapse synID of file view scoped to publication folder (`parent`)."
-  ),
-  make_option(
-    "--log_dir",
-    action = "store",
-    default = NA,
-    type = "character",
-    help = "Directory to store log files in."
-  ),
-  make_option(
-    "--task_id",
-    action = "store",
-    default = NA,
-    type = "character",
-    help = "Synapse synID of task entity for tracking success/fail."
-  ),
-  make_option(
-    "--task_table",
-    action = "store",
-    default = NA,
-    type = "character",
-    help = "Synapse synID of file view scoped to include `task_id`."
   )
 )
 opts <- parse_args(OptionParser(option_list = option_list))
 # nolint end
 
-## Create logger
-## Make sure directory exists; create if doesn't
-if (!is.na(opts$log_dir)) {
-  if (!dir.exists(opts$log_dir)) {
-    dir.create(opts$log_dir)
-  }
-  logfile_name <- glue::glue("{year(today())}-{month(today())}")
-  logpath <- glue::glue("{opts$log_dir}/{logfile_name}.log")
-  logger <- create.logger(logfile = logpath, level = "INFO")
-} else {
-  stop("No log directory given. Retry with --log_dir <directory>.")
-}
-
 ## Synapse client and logging in
 synapseclient <- reticulate::import("synapseclient")
 syntab <- reticulate::import("synapseclient.table")
 syn <- synapseclient$Synapse()
-tryCatch(
-  {
-    if(!is.na(opts$auth_token)) {
-      syn$login(authToken = opts$auth_token)
-    } else {
-      syn$login()
-    }
-  },
-  error = function(e) {
-    failure_message <- glue::glue(
-      "Log in error:\n  {e$message}"
-    )
-    error(logger, failure_message)
-    quit(status = 1)
-  }
-)
-
-## Grab annotations on task, if provided with task_id
-## If not provided with task_id, don't update
-update_task <- FALSE
-task_annots <- NA
-if (!is.na(opts$task_id)) {
-  tryCatch(
-    {
-      task_annots <<- syn$get_annotations(opts$task_id)
-      update_task <<- TRUE
-    },
-    error = function(e) {
-      # Assuming error might be from not being logged in --
-      # in this case, should exit now instead of doing more work that will fail;
-      # however, this might not be the problem and too extreme of a response.
-      failure_message <- glue::glue(
-        "Could not pull annotations from task folder:\n  {e$message}"
-      )
-      error(logger, failure_message)
-      quit(status = 1)
-    }
-  )
+if(!is.na(opts$auth_token)) {
+  syn$login(authToken = opts$auth_token)
+} else {
+  syn$login()
 }
 
 ## Grab grants ---------------------------------------------------
 
-grants <- tryCatch(
-  {
-    syn$tableQuery(
-      glue::glue(
-        "SELECT \"Grant Number\", grantSerialNumber, Program ",
-        "FROM {opts$grant_table}"
-      )
-    )$asDataFrame()
-  },
-  error = function(e) {
-    failure_message <- glue::glue(
-      "Failed at querying grant table:\n  {e$message}"
-    )
-    error(logger, failure_message)
-    if (update_task) {
-      update_task_annotation(
-        syn = syn,
-        annots = task_annots,
-        success = FALSE,
-        task_view = opts$task_table
-      )
-    }
-    quit(status = 1)
-  }
-)
+grants <- syn$tableQuery(
+  glue::glue(
+    "SELECT \"Grant Number\", grantSerialNumber, Program ",
+    "FROM {opts$grant_table}"
+  )
+)$asDataFrame()
 ## grantSerialNumber becomes the query
 ## Remove rows that have NaN or NA or empty string for the serial number
 grants <- grants[!(grants$grantSerialNumber %in% c(NaN, NA, "")), ]
@@ -206,34 +94,15 @@ grants$grantSerialNumber <- as.character(grants$grantSerialNumber)
 dat <- dplyr::right_join(grants, dat, by = "grantSerialNumber")
 ## Need to remove duplicates, but keep all grants and consortium
 ## Includes some renaming and dropping of columns
-dat <- tryCatch(
-  {
-    dat %>%
-      group_by(pmid) %>%
-      mutate(
-        grant = glue::glue_collapse(unique(.data$`Grant Number`), ", ")
-      ) %>%
-      mutate(consortium = glue::glue_collapse(unique(.data$Program), ", ")) %>%
-      select(!c(`Grant Number`, Program, grantSerialNumber)) %>%
-      rename(pubmed_id = pmid, DOI = doi, Program = consortium) %>%
-      distinct()
-  },
-  error = function(e) {
-    failure_message <- glue::glue(
-      "Failed to tidy data -- something is wrong:\n  {e$message}"
-    )
-    error(logger, failure_message)
-    if (update_task) {
-      update_task_annotation(
-        syn = syn,
-        annots = task_annots,
-        success = FALSE,
-        task_view = opts$task_table
-      )
-    }
-    quit(status = 1)
-  }
-)
+dat <- dat %>%
+  group_by(pmid) %>%
+  mutate(
+    grant = glue::glue_collapse(unique(.data$`Grant Number`), ", ")
+  ) %>%
+  mutate(consortium = glue::glue_collapse(unique(.data$Program), ", ")) %>%
+  select(!c(`Grant Number`, Program, grantSerialNumber)) %>%
+  rename(pubmed_id = pmid, DOI = doi, Program = consortium) %>%
+  distinct()
 
 ## Hacky cleaning
 ## Included in hacky_cleaning is conversion to ascii and removing html formatting
@@ -251,41 +120,9 @@ dat <- set_up_multiannotations(dat, "Program")
 ## Store publications --------------------------------------------
 
 dat_list <- purrr::transpose(dat)
-tryCatch(
-  {
-    store_as_annotations(parent = opts$parent, dat_list)
-  },
-  error = function(e) {
-    failure_message <- glue::glue(
-      "Failed to store all of the publications:\n  {e$message}"
-    )
-    error(logger, failure_message)
-    if (update_task) {
-      update_task_annotation(
-        syn = syn,
-        annots = task_annots,
-        success = FALSE,
-        task_view = opts$task_table
-      )
-    }
-    quit(status = 1)
-  }
-)
+store_as_annotations(parent = opts$parent, dat_list)
 
 ## Force file view update
 if (!is.na(opts$pub_table)) {
   syn$tableQuery(glue::glue("SELECT * FROM {opts$pub_table}"))
 }
-
-## Win -----------------------------------------------------------
-
-info(logger, "Publications updated without incident")
-if (update_task) {
-  update_task_annotation(
-    syn = syn,
-    annots = task_annots,
-    success = TRUE,
-    task_view = opts$task_table
-  )
-}
-quit(status = 0)
